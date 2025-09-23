3149ac343dfd7668f4106464b2202655fb56caf1
brahnu <nunezbrahian@gmail.com>
2025-09-23 17:27:52 -0300

    frontend funcionando

--- frontend/server.js
+++ frontend/server.js
@@ -1,1 +1,26 @@
-// SERVER FRONTEND
+import fastify from 'fastify';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fastifyStatic from '@fastify/static';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = fastify({ logger: true });
+
+server.register(fastifyStatic, {
+    root: path.join(__dirname, 'public'),
+    prefix: '/'
+});
+
+server.get('/', async (request, reply) => {
+    return reply.sendFile('index.html');
+});
+
+server.listen({ port: 4000, host: 'localhost' }, (err, address) => {
+    if (err) {
+        server.log.error(err);
+        process.exit(1);
+    }
+    console.log(`Frontend corriendo en ${address}`);
+});
