f6ae99005be0b1b12aa1f3ec5f2f6a0c6d5a18bd
Agustin <agustincigaran@icloud.com>
2025-09-23 10:12:34 -0300

    idUsuario.ts

---
+++ api/src/routes/_id_usuario/id_usuario.ts
@@ -0,0 +1,56 @@
+import { type FastifyPluginAsyncTypebox, Type } from "@fastify/type-provider-typebox"
+import { usuarioSchema } from "../../model/usuario_model.ts"
+import { PC_NotImplemented } from "../../errors/errors.ts"
+
+const usersByIdRoutes: FastifyPluginAsyncTypebox = async (fastify) => {
+
+    fastify.get("", {
+        schema:{
+            tags: ['Usuarios'],
+            summary: 'Obtener usuario',
+            description: 'Devuelve el usuario con id indicada por parametro.', 
+            params : Type.Pick(usuarioSchema, ["id_usuario"]),
+            response: {
+                200: usuarioSchema
+            }
+        }
+    },async function (request, reply){
+        throw new PC_NotImplemented()
+        // const {id_usuario} = request.params
+        // return fastify.UsersDB.getById(id_usuario)
+    })
+
+
+    fastify.put("",{
+        schema:{
+            tags: ['Usuarios'],
+            summary: 'Modificar usuario',
+            description: 'Modifica el usuario con id indicada por parametro.',  
+            body: usuarioSchema,
+            params : Type.Pick(usuarioSchema, ["id_usuario"]),
+            response: 201
+        },
+        // preHandler: fastify.verifyParamsId
+    }, async function (request, reply){
+        throw new PC_NotImplemented()
+        // fastify.UsersDB.update(request.params.id_usuario, request.body)
+        // reply.code(201).send()
+    })
+
+    fastify.delete("",{
+        schema:{
+            tags: ['Usuarios'],
+            summary: 'Borra usuario',
+            description: 'Elimina el usuario con id indicada por parametro.',  
+            params : Type.Pick(usuarioSchema, ["id_usuario"]),
+            response: 201
+        }
+    }, async function (request, reply){
+        throw new PC_NotImplemented()
+        // const {id_usuario} = request.params
+        // fastify.UsersDB.delete(id_usuario)
+        reply.code(201).send()
+    })
+}
+
+export default usersByIdRoutes
