10421407a991af64c86552899594b778ed60bc9c
Agustin <agustincigaran@icloud.com>
2025-09-23 10:06:38 -0300

    update serverTs

--- api/server.ts
+++ api/server.ts
@@ -1,41 +1,46 @@
+import type { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
 import fastifyFormbody from "@fastify/formbody";
-import fastifyStatic from "@fastify/static";
+// import fastifyStatic from "@fastify/static";
 import { dirname, join } from 'node:path';
-import {PC_NotImplemented} from "./errors/errors.ts"
+import { fileURLToPath } from 'node:url'
+import autoLoad from '@fastify/autoload'
 import cors from "@fastify/cors";
 import fastify from 'fastify';
 
-const server = fastify({logger:true});
-const ruta = join(dirname(process.argv[1]), "../frontend/public");
+const archivo = fileURLToPath(import.meta.url)
+const ruta = join(dirname(__filename), 'src')
+
+const server = fastify({logger:true}).withTypeProvider<TypeBoxTypeProvider>();
 
 await server.register(fastifyFormbody);
+
 await server.register(cors, {
     origin: "http://localhost:4000",
     methods: ["GET", "POST", "PUT", "DELETE"]
 });
-await server.register(fastifyStatic, {
-root: ruta,
-prefix: "/"
-});
 
-server.get("/usuarios", async(req, rep) => {
-    throw new PC_NotImplemented()
+await server.register(autoLoad, {
+    dir: join(ruta, 'plugins')
+})
+await server.register(autoLoad, {
+    dir: join(ruta, 'decorators')
 })
 
-server.post("/usuarios", async(req, rep) => {
-    throw new PC_NotImplemented()
+server.register(autoLoad, {
+    dir: join(ruta, 'routes'),
+    routeParams: true
 })
 
-server.put("/usuarios", async(req, rep) => {
-    throw new PC_NotImplemented()
-})
+const port = Number(process.env.API_PORT) || 4000;
+const host = '::';
 
-server.delete("/usuarios", async(req, rep) => {
-    throw new PC_NotImplemented()
-})
+// await server.register(fastifyStatic, {
+// root: ruta,
+// prefix: "/"
+// });
 
 try{
-    await server.listen({port: 3000})
+    await server.listen({ port, host });
 } catch(err) {
     server.log.error(err);
     process.exit(1);
