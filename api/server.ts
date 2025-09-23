0546f4b4f9633d5f0a04f6966f78f34207971eb5
Agustin <agustincigaran@icloud.com>
2025-09-23 09:53:31 -0300

    serverTs

--- api/server.ts
+++ api/server.ts
@@ -1,1 +1,41 @@
-// SERVIDOR DE LA API
+import fastifyFormbody from "@fastify/formbody";
+import fastifyStatic from "@fastify/static";
+import { dirname, join } from 'node:path'
+import cors from "@fastify/cors";
+import fastify from 'fastify';
+
+const server = fastify({logger:true});
+const ruta = join(dirname(process.argv[1]), "../frontend/public");
+
+await server.register(fastifyFormbody);
+await server.register(cors, {
+    origin: "http://localhost:4000",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+});
+await server.register(fastifyStatic, {
+root: ruta,
+prefix: "/"
+});
+
+server.get("/usuarios", async(req, rep) => {
+    console.log("not implemented")
+})
+
+server.post("/usuarios", async(req, rep) => {
+    console.log("not implemented")
+})
+
+server.put("/usuarios", async(req, rep) => {
+    console.log("not implemented")
+})
+
+server.delete("/usuarios", async(req, rep) => {
+    console.log("not implemented")
+})
+
+try{
+    await server.listen({port: 3000})
+} catch(err) {
+    server.log.error(err);
+    process.exit(1);
+}
