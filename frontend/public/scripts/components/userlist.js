9a55e6fc3ab78b10f6e3baaaec14223a94485cab
Mati <mperezfrontan@gmail.com>
2025-09-24 18:00:51 -0300

    some css

--- frontend/public/scripts/components/userlist.js
+++ frontend/public/scripts/components/userlist.js
@@ -7,18 +7,18 @@ export async function printUserList() {
     const users = await getUsers()
 
     try {
-        let usersHtml = `<h2>Listado de Usuarios</h2>`;
+        let usersHtml = `<h2 class="text-2xl font-semibold tracking-tight mb-10 text-balance text-white text-center">Listado de Usuarios</h2>`;
             
         if (!users || users.length === 0) {
             usersHtml += `<p>No hay usuarios registrados.</p>`;
         } else {
-            usersHtml += `<ul>`;
+            usersHtml += `<ul class="flex max-w-4xl m-auto p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5 flex-col justify-between gap-x-6 py-5">`;
             users.forEach(user => {
                 usersHtml += `
-                    <li>
+                    <li class="flex justify-between text-cennter gap-x-6 py-5">
                         ${user.nombres} ${user.username} - ${user.email}
-                        <button class="edit-btn" data-id="${user.id_usuario}">Editar</button>
-                        <button class="delete-btn" data-id="${user.id_usuario}">Eliminar</button>                        
+                        <button class="edit-btn mt-6 cursor-pointer block rounded-lg px-3 py-2 text-base/7 font-semibold text-white bg-yellow-600 hover:bg-white/5" data-id="${user.id_usuario}">Editar</button>
+                        <button class="delete-btn mt-6 cursor-pointer  block rounded-lg px-3 py-2 text-base/7 font-semibold text-white bg-red-600 hover:bg-white/5" data-id="${user.id_usuario}">Eliminar</button>                        
                     </li>
                 `;
             });
--- frontend/public/scripts/templates/creationFormTemplate.js
+++ frontend/public/scripts/templates/creationFormTemplate.js
@@ -1,23 +1,36 @@
 // FUNCION PARA DEVOLVER EL HTML TEMPLATE DEL FORM DE CREACION DE USUARIOS 
-
+const labelClass = "block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+const inputClass = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
 export const creationFormTemplate = `
-        <h2 id="form-title">Creación de un nuevo usuario</h2>
-        <form id="create-user-form">
-            <label for="nombres">Nombres:</label>
-            <input type="text" id="nombres" name="nombres" required>
-            <br><br>
-            <label for="is_admin">Administrador:</label>
-            <input type="checkbox" id="is_admin">
-            <br><br>
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required>
-            <br><br>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-            <br><br>
-            <button type="submit" id="submit-btn">Guardar</button>
-            <button type="button" id="cancel-btn">Cancelar</button>
+    <h2 id="form-title" class="text-2xl font-semibold tracking-tight mb-10 text-balance text-white text-center">Creación de un nuevo usuario</h2>
+    <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5 max-w-4xl m-auto">
+        <form id="create-user-form" class="grid gap-4 mb-4 sm:grid-cols-2">
+        <div>
+            <label for="nombres" class="${labelClass}" >Nombres:</label>
+            <input type="text" id="nombres" name="nombres" class="${inputClass}" required>
+        </div>
+        <div>
+            <label for="is_admin" class="${labelClass}">Administrador:</label>
+            <input type="checkbox" id="is_admin" >
+        </div>
+        <div>
+            <label for="username" class="${labelClass}">Username:</label>
+            <input type="text" id="username" name="username" class="${inputClass}" required>
+        </div>
+        <div>
+            <label for="email" class="${labelClass}">Email:</label>
+            <input type="email" id="email" name="email" class="${inputClass}" required>
+        </div>
+        <div class="justify-self-center">
+            <button type="submit" id="submit-btn" class="mt-6 cursor-pointer -mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white bg-green-600 hover:bg-white/5">Guardar</button>
+        </div>
+        <div class="justify-self-center">
+            <button type="button" id="cancel-btn" class="mt-6 cursor-pointer -mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white bg-red-600 hover:bg-white/5">Cancelar</button>
+        </div>
+            
+            
         </form>
         <br>
         <br>
+    </div>
     `
