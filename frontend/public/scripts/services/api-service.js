2ee4cf897d1ff3d51fb7776aba042b52a4fc255a
Mati <mperezfrontan@gmail.com>
2025-09-25 21:51:02 -0300

    separacion en las responsabilidades de la api

---
+++ frontend/public/scripts/services/api-service.js
@@ -0,0 +1,60 @@
+export async function get(url){
+    const response = await fetch(url);
+    
+    if (!response.ok) {
+        throw new Error('Error al obtener la lista de usuarios');
+    }
+
+    const data = await response.json();
+    return data
+}
+
+
+
+
+export async function post(url, body){
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+
+    if (!response.ok) {
+        throw new Error('Error al crear usuario');
+    }
+
+    const data = await response.json();
+    return data
+}
+
+
+
+
+export async function put(url, body){
+    const response = await fetch(url, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+    });
+
+    if (!response.ok) {
+        throw new Error('Error al actualizar usuario')
+    }
+
+    return await response.json();
+}
+
+
+
+
+export async function del(url){
+    const response = await fetch(url, {
+        method: 'DELETE'
+    });
+    
+    if (!response.ok) {
+        throw new Error('Error al eliminar usuario.');
+    }
+}
