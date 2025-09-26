c3b37dd2ffb619864b54f192de8b8bd398ad7598
Mati <mperezfrontan@gmail.com>
2025-09-25 22:31:20 -0300

    comments about css and db

--- frontend/public/scripts/components/userlist.js
+++ frontend/public/scripts/components/userlist.js
@@ -12,13 +12,16 @@ export async function printUserList() {
         if (!users || users.length === 0) {
             usersHtml += `<p>No hay usuarios registrados.</p>`;
         } else {
-            usersHtml += `<ul class="flex max-w-4xl m-auto p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5 flex-col justify-between gap-x-6 py-5">`;
+            usersHtml += `<ul class="flex max-w-4xl m-auto p-4 rounded-lg shadow bg-gray-800 sm:p-5 flex-col justify-between gap-x-6 py-5">`;
             users.forEach(user => {
                 usersHtml += `
-                    <li class="flex justify-between text-cennter gap-x-6 py-5">
-                        ${user.nombres} ${user.username} - ${user.email}
-                        <button class="edit-btn mt-6 cursor-pointer block rounded-lg px-3 py-2 text-base/7 font-semibold text-white bg-yellow-600 hover:bg-white/5" data-id="${user.id_usuario}">Editar</button>
-                        <button class="delete-btn mt-6 cursor-pointer  block rounded-lg px-3 py-2 text-base/7 font-semibold text-white bg-red-600 hover:bg-white/5" data-id="${user.id_usuario}">Eliminar</button>                        
+                    <li class="flex justify-left w-full text-center gap-x-6 py-5">
+                        <div class="w-full text-left flex flex-col">
+                            <h2 class="text-xl">${user.nombres}</h2>
+                            <p class="text-md text-gray-400">${user.username} - ${user.email}</p>
+                        </div>
+                        <button class="edit-btn cursor-pointer w-1/6 ml-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white bg-yellow-600 hover:bg-white/5" data-id="${user.id_usuario}">Editar</button>
+                        <button class="delete-btn cursor-pointer w-1/6 ml-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white bg-red-600 hover:bg-white/5" data-id="${user.id_usuario}">Eliminar</button>                        
                     </li>
                 `;
             });
@@ -47,7 +50,7 @@ export async function printUserList() {
                 const userId = button.dataset.id;
                 const users = await getUsers();
                 const user = users.find(u => u.id_usuario == userId);
-                editUserForm(user); // Abrir form de edición
+                editUserForm(user);
             });
         });
 
--- frontend/public/scripts/templates/creationFormTemplate.js
+++ frontend/public/scripts/templates/creationFormTemplate.js
@@ -1,9 +1,9 @@
 // FUNCION PARA DEVOLVER EL HTML TEMPLATE DEL FORM DE CREACION DE USUARIOS 
-const labelClass = "block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-const inputClass = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+const labelClass = "block mb-2 text-sm font-medium text-white"
+const inputClass = "border text-sm rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
 export const creationFormTemplate = `
     <h2 id="form-title" class="text-2xl font-semibold tracking-tight mb-10 text-balance text-white text-center">Creación de un nuevo usuario</h2>
-    <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5 max-w-4xl m-auto">
+    <div class="relative p-4 rounded-lg shadow bg-gray-800 sm:p-5 max-w-4xl m-auto">
         <form id="create-user-form" class="grid gap-4 mb-4 sm:grid-cols-2">
         <div>
             <label for="nombres" class="${labelClass}" >Nombres:</label>
